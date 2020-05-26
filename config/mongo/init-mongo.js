15dade416b448c2f7f6a197b3770b75ccc3d14e7
yangxg <xiangguo.yang@gmail.com>
2020-05-26 16:41:06 +0800

    update redis version

--- config/mongo/init-mongo.js
+++ config/mongo/init-mongo.js
@@ -6,6 +6,10 @@ db.createUser(
             {
                 role: "readWrite",
                 db: "springboot"
+            },
+            {
+                "role" : "dbAdmin",
+                "db" : "springboot"
             }
         ]
     }
